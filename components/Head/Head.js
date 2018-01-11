d3256b494a383ff84d3bb7af7bcd524b8603cd52
xuxuejian <jearbilove@yeah.net>
2018-01-11 19:06:27 +0800

    update

---
+++ components/Head/Head.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default () => {
+    return (
+        <Head>
+            <title>Next demo</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+    )
+}
---
+++ pages/hello.js
@@ -0,0 +1,22 @@
+import Person from './Person';
+import Link from 'next/link'
+
+const Index = ({name, age}) => {
+    return (
+        <div className="my-root">
+            <Person />
+            <Link href="/about">
+                <a>about</a>
+            </Link>
+        </div>
+    )
+}
+
+Index.getInitialProps = async (options) => {
+    return {
+        name: 'callie',
+        age: 18
+    }
+}
+
+export default Index
